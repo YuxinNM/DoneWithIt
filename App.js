466f8c8d674ef87d586aa091c9995114e738ac93
Nancy <yma60@student.ubc.ca>
2025-09-02 10:23:17 -0700

    update with expo

--- App.js
+++ App.js
@@ -2,7 +2,6 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-    // let x = 1;
     // console.log("executed");
  
   return (
